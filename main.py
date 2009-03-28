11616aa2ac38a97d6a368d65cadfd03a7d436e1c
Per Borgman <sterdo@gmail.com>
2009-03-28 02:40:42 +0000

    drawing untextured tilemap

--- main.py
+++ main.py
@@ -12,13 +12,29 @@ import os
 from pyglet import window
 from pyglet import app
 from pyglet import clock
+from pyglet.gl import *
 import controller
 
 win = window.Window(640, 480, "DEATHTROID")
 
 @win.event
+def on_resize(width, height):
+  glViewport(0, 0, width, height)
+  glMatrixMode(gl.GL_PROJECTION)
+  glLoadIdentity()
+  glOrtho(0, 20, 15, 0, -1, 1)
+  glMatrixMode(gl.GL_MODELVIEW)
+  return pyglet.event.EVENT_HANDLED
+
+@win.event
 def on_draw():
-    win.clear()
+    #win.clear()
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    
+    game_controller.draw()
+    
+    
     
 
 
