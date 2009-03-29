794951017ed9ee7ab2796f5ab4ec5b9c79f60282
Joachim Bengtsson <joachimb@gmail.com>
2009-03-29 01:20:27 +0000

    twisted in network.py only
    git-svn-id: svn://nevyn.nu/deathtroid/trunk@70 65469f3e-e0e0-476a-bd21-a889408ea2c8

--- network.py
+++ network.py
@@ -1,11 +1,14 @@
 import twisted
+from twisted.internet.protocol import Protocol, Factory, ClientFactory
 from twisted.protocols.basic import NetstringReceiver
+import pygletreactor
+pygletreactor.install() # <- this must come before...
+from twisted.internet import reactor, task # <- ...importing this reactor!
+
 import struct
 import demjson
 
 class DeathtroidProtocol(NetstringReceiver):
-    
-  
   def connectionMade(self):
     print "connection made"
     self.controller.newConnection(self)
@@ -28,4 +31,37 @@ class DeathtroidProtocol(NetstringReceiver):
     dd = demjson.encode(d).encode("utf8")
     
     self.sendString(dd)
+
+class DeathtroidServerFactory (Factory):
+
+  protocol = DeathtroidProtocol
+
+  def __init__(self, controller):
+    self.controller = controller
+
+  def buildProtocol(self, addr):
+    newProto = Factory.buildProtocol(self, addr)
+    newProto.controller = self.controller
+    return newProto
+
+
+class DeathtroidClientFactory (ClientFactory):
+
+  protocol = DeathtroidProtocol
+
+  def __init__(self, controller):
+    self.controller = controller
+
+  def buildProtocol(self, addr):
+    newProto = ClientFactory.buildProtocol(self, addr)
+    newProto.controller = self.controller
+    return newProto
+
+
+
+def startServer(port, ctrlr):
+  reactor.listenTCP(port, DeathtroidServerFactory(ctrlr))
+  
+def startClient(host, port, ctrlr):
+  reactor.connectTCP(host, port, DeathtroidClientFactory(ctrlr))
 
