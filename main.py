e7de913fda338d09b4b65cc22d7cd799b44d48a5
Amanda Rösler <amanda.rosler@gmail.com>
2009-03-29 14:55:35 +0000

    Added a menu
    git-svn-id: svn://nevyn.nu/deathtroid/trunk@106 65469f3e-e0e0-476a-bd21-a889408ea2c8

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from pyglet.window import key
 
 import client_controller
 import server_controller
+import menu_controller
 import logging
 import euclid
 import demjson
@@ -30,53 +31,90 @@ logging.basicConfig(level=logging.WARNING)
 
 win = window.Window(640, 480, "DEATHTROID")
 
-current_controller = None
+
+def start_game(controller, player_name = "Samus", host="localhost"):
+  
+  global menu, server, client
+  menu = None
+    
+  if controller == "server" or controller == "both":
+    if server == None:
+      server = server_controller.ServerController()
+    clock.schedule_interval(server.update, 1./30.)
+    
+  if controller == "client" or controller == "both":
+    if client == None:
+      client = client_controller.ClientController(player_name, host)
+    clock.schedule(client.update)
+    
+
+def start_menu():
+  
+    global menu, server, client, win
+    server = None
+    client = None
+    if menu == None:
+      menu = menu_controller.MenuController(win)
+    clock.schedule(menu.update)
+    
+    
 
 @win.event
 def on_resize(width, height):
-  if game_controller:
+  if client:
     glViewport(0, 0, width, height)
     glMatrixMode(gl.GL_PROJECTION)
     glLoadIdentity()
     glOrtho(0, 20, 15, 0, -1, 1)
     glMatrixMode(gl.GL_MODELVIEW)
-    return pyglet.event.EVENT_HANDLED
+    
+  elif menu:
+    menu.resize(width, height)
+  
+  return pyglet.event.EVENT_HANDLED
 
 @win.event
 def on_draw():
-  if game_controller:
-    game_controller.draw()
+  if client:
+    client.draw()
+  elif menu:
+    win.clear()
+    menu.draw()
 
 @win.event
 def on_key_press(symbol, modifiers):
-  if game_controller:
+  if client:
     event = None
     
     if symbol == key.LEFT:
-      game_controller.action("move_left")
+      client.action("move_left")
     elif symbol == key.RIGHT:
-      game_controller.action("move_right")
+      client.action("move_right")
       
     elif symbol == key.Z:
-      game_controller.action("jump")
+      client.action("jump")
       
     elif symbol == key.X:
-      game_controller.action("fire")
+      client.action("fire")
+      
+  elif menu:
+    if symbol == key.ENTER:
+      menu.keyboard_event("pressed_enter")
 
 @win.event
 def on_key_release(symbol, modifiers):
   global fullscreen
-  if game_controller:
+  if client:
     if symbol == key.LEFT:
-      game_controller.action("stop_moving_left")
+      client.action("stop_moving_left")
     elif symbol == key.RIGHT:
-      game_controller.action("stop_moving_right")
+      client.action("stop_moving_right")
       
     elif symbol == key.Z:
-      game_controller.action("stop_jump")
+      client.action("stop_jump")
       
     elif symbol == key.X:
-      game_controller.action("stop_fire")
+      client.action("stop_fire")
       
       
     elif symbol == key.F:
@@ -103,38 +141,51 @@ fullscreen = False
 
 current_tile = 0
 num_tiles = 14
+
   
 @win.event
 def on_mouse_press(x, y, button, modifiers):
-  global current_tile
-  y = 480 - y
+  
+  if menu:
+    if button == pyglet.window.mouse.LEFT:
+      menu.mouse_pressed(x, y)
+  
+  elif client and server:
+    global current_tile
+    y = 480 - y
 
-  vp = euclid.Vector2(game_controller.view.cam.x*32, game_controller.view.cam.y*32)
+    vp = euclid.Vector2(client.view.cam.x*32, client.view.cam.y*32)
 
-  x += vp.x
-  y += vp.y
+    x += vp.x
+    y += vp.y
 
-  x = int(x)
-  y = int(y)
+    x = int(x)
+    y = int(y)
 
-  x /= 32
-  y /= 32
+    x /= 32
+    y /= 32
 
-  print "cam: ", game_controller.view.cam
+    print "cam: ", client.view.cam
 
-  #x += game_controller.view.cam.x
-  #y += int(game_controller.view.cam.y)
+    #x += client.view.cam.x
+    #y += int(client.view.cam.y)
 
 
-  print "sätt i ", x, y
-  if button == window.mouse.LEFT:
+    print "sätt i ", x, y
+    if button == window.mouse.LEFT:
     
-    server.game.level.main_layer.tilemap.map[y][x] = (server.game.level.main_layer.tilemap.map[y][x] - 1) % num_tiles
-    game_controller.game.level.main_layer.tilemap.map[y][x] = (game_controller.game.level.main_layer.tilemap.map[y][x] - 1) % num_tiles
+      server.game.level.main_layer.tilemap.map[y][x] = (server.game.level.main_layer.tilemap.map[y][x] - 1) % num_tiles
+      client.game.level.main_layer.tilemap.map[y][x] = (client.game.level.main_layer.tilemap.map[y][x] - 1) % num_tiles
     
-  elif button == window.mouse.RIGHT:    
-    server.game.level.main_layer.tilemap.map[y][x] = (server.game.level.main_layer.tilemap.map[y][x] + 1) % num_tiles
-    game_controller.game.level.main_layer.tilemap.map[y][x] = (game_controller.game.level.main_layer.tilemap.map[y][x] + 1) % num_tiles    
+    elif button == window.mouse.RIGHT:    
+      server.game.level.main_layer.tilemap.map[y][x] = (server.game.level.main_layer.tilemap.map[y][x] + 1) % num_tiles
+      client.game.level.main_layer.tilemap.map[y][x] = (client.game.level.main_layer.tilemap.map[y][x] + 1) % num_tiles    
+
+
+@win.event
+def on_mouse_motion(x, y, dx, dy):
+  if menu:
+    menu.mouse_moved(x, y)
 
 
 glPointSize(5)
@@ -144,23 +195,34 @@ glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 
 glEnable(GL_TEXTURE_2D)
 
-if(len(sys.argv) < 3):
+server = client = menu = None
+
+if(len(sys.argv) < 3 and len(sys.argv) != 1):
   print "Usage: python main.py {playerName} [server|client {host}|both]"
+  
+elif (len(sys.argv) == 3):
 
-roles = sys.argv[2]
-name = sys.argv[1]
+  roles = sys.argv[2]
+  name = sys.argv[1]
+  if(roles == "client"): 
+    host = sys.argv[3]
+    start_game(roles, name, host)
+  else:
+    start_game(roles, name)
+  
 
-server = game_controller = None
+  #if(roles == "server" or roles == "both"):
+  #  server = server_controller.ServerController()
+  #  clock.schedule_interval(server.update, 1./30.)
 
-if(roles == "server" or roles == "both"):
-  server = server_controller.ServerController()
-  clock.schedule_interval(server.update, 1./30.)
-
-if(roles == "client" or roles == "both"):
-  host = "localhost"
-  if(roles == "client"): host = sys.argv[3]
-  game_controller = client_controller.ClientController(name, host)
-  clock.schedule(game_controller.update)
+  #if(roles == "client" or roles == "both"):
+  #  host = "localhost"
+  #  if(roles == "client"): host = sys.argv[3]
+  #  client = client_controller.ClientController(name, host)
+  #  clock.schedule(client.update)
+    
+else:
+  start_menu()
 
 
 #reactor.run()
